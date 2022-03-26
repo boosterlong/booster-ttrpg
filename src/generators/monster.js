bddca50f30583ef38f47457f1a986d3a1b22a218
Booster <chartrandblake@hotmail.com>
2022-03-26 14:55:41 -0700

    Refactored the monster card out of the randomize function

--- src/generators/monster.js
+++ src/generators/monster.js
@@ -8,22 +8,11 @@ import HikingIcon from '@mui/icons-material/Hiking';
 import PoolIcon from '@mui/icons-material/Pool';
 import FlightIcon from '@mui/icons-material/Flight';
 
-export default function GetRandomMonster() {
+export function monsterBlock(monster) {
 
-  const [monster, setMonster] = useState('')
-
-  async function getRandomMonster() {
-    const response = await fetch('https://blakechartrand.com/5e-SRD-Monsters.json');
-    const monster = await response.json();
-    const monsterStatBlock = monster[getRandomInt(monster.length)];
-    return monsterStatBlock
-  }
-
-  async function randomlyGenerateMonster() {
-    let randoMonster = await getRandomMonster()
-    let actions, specialAbilities, walk, climb, swim, fly = null
+  let actions, specialAbilities, walk, climb, swim, fly = null
     try {
-      specialAbilities = randoMonster.special_abilities.map(ability =>
+      specialAbilities = monster.special_abilities.map(ability =>
           <div className="ability-desc">
             <Typography variant="h6">
               {ability.name}
@@ -36,7 +25,7 @@ export default function GetRandomMonster() {
     { specialAbilities = 'No special abilities.'}
 
     try {
-      actions = randoMonster.actions.map(actions =>
+      actions = monster.actions.map(actions =>
           <div className="ability-desc">
             <Typography variant="h6">
               {actions.name}
@@ -48,85 +37,102 @@ export default function GetRandomMonster() {
     } catch
     { actions = 'Could not load action data.'}
 
-    if (randoMonster.speed.walk) {
-      walk = randoMonster.speed.walk
+    if (monster.speed.walk) {
+      walk = monster.speed.walk
     } else {
       walk = '0 ft.'
     }
 
-    if (randoMonster.speed.climb) {
-      climb = randoMonster.speed.climb
+    if (monster.speed.climb) {
+      climb = monster.speed.climb
     } else {
       climb = '0 ft.'
     }
 
-    if (randoMonster.speed.swim) {
-      swim = randoMonster.speed.swim
+    if (monster.speed.swim) {
+      swim = monster.speed.swim
     } else {
       swim = '0 ft.'
     }
 
-    if (randoMonster.speed.fly) {
-      fly = randoMonster.speed.fly
+    if (monster.speed.fly) {
+      fly = monster.speed.fly
     } else {
       fly = '0 ft.'
     }
 
-    setMonster(
-    <div>
-{/* Name / Type / HP / AC */}
-      <Typography variant="h5">
-        {randoMonster.name}
-      </Typography>
-      <Typography className="capitalize" variant="subtitle2">
-        {randoMonster.size} {randoMonster.type}
-      </Typography>
-      <Typography className="capitalize" variant="subtitle2">
-        {randoMonster.alignment} {randoMonster.subtype}
-      </Typography>
-      <Typography className="inline-center v-padding-1" variant="h5">
-        {randoMonster.hit_points}<HealthAndSafetyIcon /> {randoMonster.armor_class}<ShieldIcon />
-      </Typography>
-      <Typography className="inline-center v-padding-1" variant="h6">
-        |{walk}<DirectionsRunIcon />|
-        {climb}<HikingIcon />|
-        {swim}<PoolIcon />|
-        {fly}<FlightIcon />|
-      </Typography>
-{/* Ability Scores */}
-      <Typography style={{display:'flex',flexDirection:'row'}} variant="subtitle1">
-        <span className="stat-box tooltip"><span>Str</span>{getScoreBonusString(randoMonster.strength)}
-          <span className="tooltiptext">{randoMonster.strength}</span>
-        </span>
-        <span className="stat-box tooltip"><span>Dex</span>{getScoreBonusString(randoMonster.dexterity)}
-          <span className="tooltiptext">{randoMonster.dexterity}</span>
-        </span>
-        <span className="stat-box tooltip"><span>Con</span>{getScoreBonusString(randoMonster.constitution)}
-          <span className="tooltiptext">{randoMonster.constitution}</span>
-        </span>
-        <span className="stat-box tooltip"><span>Wis</span>{getScoreBonusString(randoMonster.wisdom)}
-          <span className="tooltiptext">{randoMonster.wisdom}</span>
-        </span>
-        <span className="stat-box tooltip"><span>Int</span>{getScoreBonusString(randoMonster.intelligence)}
-          <span className="tooltiptext">{randoMonster.intelligence}</span>
-        </span>
-        <span className="stat-box tooltip"><span>Cha</span>{getScoreBonusString(randoMonster.charisma)}
-          <span className="tooltiptext">{randoMonster.charisma}</span>
-        </span>
-      </Typography>
-{/* Attacks */}
-      <Typography>
-        {actions}
-      </Typography>
-      <Typography>
-        {specialAbilities}
-      </Typography>
-    </div>)
+  return (<div>
+    {/* Name / Type / HP / AC / Speed */}
+          <Typography variant="h5">
+            {monster.name}
+          </Typography>
+          <Typography className="capitalize" variant="subtitle2">
+            {monster.size} {monster.type}
+          </Typography>
+          <Typography className="capitalize" variant="subtitle2">
+            {monster.alignment} {monster.subtype}
+          </Typography>
+          <Typography className="inline-center v-padding-1" variant="h5">
+            {monster.hit_points}<HealthAndSafetyIcon /> {monster.armor_class}<ShieldIcon />
+          </Typography>
+          <Typography className="inline-center v-padding-1" variant="h6">
+            |{walk}<DirectionsRunIcon />|
+            {climb}<HikingIcon />|
+            {swim}<PoolIcon />|
+            {fly}<FlightIcon />|
+          </Typography>
+    {/* Ability Scores */}
+          <Typography style={{display:'flex',flexDirection:'row'}} variant="subtitle1">
+            <span className="stat-box tooltip"><span>Str</span>{getScoreBonusString(monster.strength)}
+              <span className="tooltiptext">{monster.strength}</span>
+            </span>
+            <span className="stat-box tooltip"><span>Dex</span>{getScoreBonusString(monster.dexterity)}
+              <span className="tooltiptext">{monster.dexterity}</span>
+            </span>
+            <span className="stat-box tooltip"><span>Con</span>{getScoreBonusString(monster.constitution)}
+              <span className="tooltiptext">{monster.constitution}</span>
+            </span>
+            <span className="stat-box tooltip"><span>Wis</span>{getScoreBonusString(monster.wisdom)}
+              <span className="tooltiptext">{monster.wisdom}</span>
+            </span>
+            <span className="stat-box tooltip"><span>Int</span>{getScoreBonusString(monster.intelligence)}
+              <span className="tooltiptext">{monster.intelligence}</span>
+            </span>
+            <span className="stat-box tooltip"><span>Cha</span>{getScoreBonusString(monster.charisma)}
+              <span className="tooltiptext">{monster.charisma}</span>
+            </span>
+          </Typography>
+    {/* Attacks */}
+          <Typography>
+            {actions}
+          </Typography>
+          <Typography>
+            {specialAbilities}
+          </Typography>
+        </div>)
+}
+
+export default function RandomMonster() {
+
+  const [randomMonster, setRandomMonster] = useState('')
+
+  async function getRandomMonster() {
+    const response = await fetch('https://blakechartrand.com/5e-SRD-Monsters.json');
+    const monster = await response.json();
+    const monsterData = monster[getRandomInt(monster.length)];
+    return monsterData
+  }
+
+  async function randomlyGenerateMonster() {
+    let randoMonster = await getRandomMonster()
+    
+    setRandomMonster(monsterBlock(randoMonster)
+    )
   }
 
   return (
     <Container className="generator-card" maxWidth="md">
-      {monster}
+      {randomMonster}
       <Button variant="contained" onClick={()=> randomlyGenerateMonster()}>Generate Random Monster</Button>
     </Container>)
 }
